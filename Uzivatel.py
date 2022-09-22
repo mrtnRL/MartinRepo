e20fbccae850c2e0b55b7e9b811b27e09c6c20dd
Martin <m.opalek1@gmail.com>
2022-09-22 15:34:42 +0200

    init

---
+++ Uzivatel.py
@@ -0,0 +1,10 @@
+class uzivatel:
+    def __init__(self, first_name, last_name, age) -> None:
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+        pass
+
+    def print_info(self) -> None:
+        print(f'Meno: {self.first_name}, Priezvisko: {self.last_name}, Vek: {self.age}')
